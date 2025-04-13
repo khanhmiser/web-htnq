d4cd0f48bc9c39aebac2bd4796e22e85c9606e05
Hoàng Quốc Khánh <hoangquockhanhxinchao@gmail.com>
2025-04-13 14:55:29 +0700

    chỉnh mysql client

--- htnq/settings.py
+++ htnq/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,6 +76,7 @@ WSGI_APPLICATION = 'htnq.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
 
 DATABASES = {
     "default": {
